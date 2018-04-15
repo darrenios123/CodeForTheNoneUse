eab5df89f5c71e8b426ce0dc299b105f6fad9308
darren <darrenios123@gmail.com>
2018-04-15 14:22:07 +0800

    1
    add all code to git

---
+++ MyApp/platforms/ios/untils/GuidePagesViewController.h
@@ -0,0 +1,12 @@
+//
+//  GuidePagesViewController ViewController.h
+//  HelloCordova
+//
+//  Created by Darren on 2018/4/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GuidePagesViewController: UIViewController
+
+@end
